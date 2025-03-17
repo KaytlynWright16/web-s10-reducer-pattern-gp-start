7a2269b3b72260d5084d8df56a5e950b01ae5142
Kaytlyn Wright <kaytlynwright16@gmail.com>
2025-03-17 13:36:16 -0400

    app.js done

--- frontend/components/TodoForm.js
+++ frontend/components/TodoForm.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const CHANGE_LABEL = 'CHANGE_LABEL';
+const CHANGE_IS_COMPLETED = 'CHANGE_IS_COMPLETED';
+
+const initialState = {
+  todoLabel: '',
+  todoIsCompleted: false
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case CHANGE_LABEL:
+      return { ...state, todoLabel: action.payload };
+    case CHANGE_IS_COMPLETED: 
+      return { ...state, todoIsCompleted: action.payload }
+    default:
+      return state;
+  }
+};
 
 export default function TodoForm() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+  
   return (
     <form id="todoForm">
       <h3>New Todo Form</h3>
       <label><span>Todo label:</span>
         <input
+          onChange={onLabelChange}
+          value={state.todoLabel}
           type='text'
           name='todoLabel'
           placeholder='Type label'
@@ -13,12 +38,14 @@ export default function TodoForm() {
       </label>
       <label><span>Is completed:</span>
         <input
+          onChange={onIsCompletedChange}
           type='checkbox'
           name='todoIsCompleted'
         />
       </label>
       <label><span>Create todo:</span>
         <input
+          checked={state.todoIsCompleted}
           type='submit'
           value='Do it!'
         />
